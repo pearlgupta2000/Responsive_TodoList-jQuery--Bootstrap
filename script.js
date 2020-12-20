6e549d77789d714ed704f9cf511a0ff70f7b10ef
Pearl Gupta <44175292+pearlgupta2000@users.noreply.github.com>
2020-12-21 00:58:35 +0530

    Updated functionalities

--- script.js
+++ script.js
@@ -5,18 +5,56 @@ let add=$('#add')
 let reset=$('#reset')
 let sort=$('#sort')
 let cleanup=$('#cleanup')
+let inTaskDes=$('#inTaskDes')
+
+function date(){
+    var today = new Date();
+    var dd = today.getDate();
+
+    var mm = today.getMonth()+1; 
+    var yyyy = today.getFullYear();
+    if(dd<10) 
+    {
+        dd='0'+dd;
+    } 
+
+    if(mm<10) 
+    {
+        mm='0'+mm;
+    } 
+    today = dd+'/'+mm+'/'+yyyy;
+    return today
+}
 
 function addItem(){
-    let item=$(`<li>`,{
-        'class':'list-group-item',
+    let a=$(`<a>`,{
+        'href':'#',
+        'class':'list-group-item list-group-item-action list-group-item-success'
+    })
+    let item=$(`<div>`,{
+        'class':'d-flex w-100 justify-content-between'
+    })
+    let heading_=$(`<h4>`,{
+        'class':'mb-1',
         text:inTask.val()
     })
-
-    item.click((ev)=>{
+    let date_=$(`<small>`,{
+        text:date()
+    })
+    let para=$(`<p>`,{
+        'class':'mb-1',
+        text:inTaskDes.val()
+    })
+    a.click((ev)=>{
         $(ev.currentTarget).toggleClass('done')
     }) 
-    ulTask.append(item)
+    item.append(heading_)
+    item.append(date_)
+    a.append(item)
+    a.append(para)
+    ulTask.append(a)
     inTask.val('')
+    inTaskDes.val('')
     ToggleButton()
 }
 
@@ -30,20 +68,36 @@ function Sortlist(){
 }
 
 function ToggleButton(){
-    reset.prop('disabled',inTask.val()=='')
+    if(inTask.val()!=''){
+        reset.prop('disabled',false)
+    }
+    else if(inTaskDes.val()!=''){
+        reset.prop('disabled',false)
+    }
+    else {
+        reset.prop('disabled',true)
+    }
     add.prop('disabled',inTask.val()=='')
     sort.prop('disabled',ulTask.children().length<1)
     cleanup.prop('disabled',ulTask.children().length<1)
 }
 inTask.on('input',ToggleButton)
+inTaskDes.on('input',ToggleButton)
 
 inTask.keypress((e)=>{
    if(e.which==13) addItem()
 })
 
+inTaskDes.keypress((e)=>{
+    if(inTask.val()!=''){
+    if(e.which==13) addItem()
+    }
+ })
+
 add.click(addItem)
 reset.click(()=>{
     inTask.val('')
+    inTaskDes.val('')
     ToggleButton()
 })
 cleanup.click(clearDone)
